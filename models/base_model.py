19410a1a99495923b0d47138e551e08d56065732
Franklin-tech01 <franklinmike2021@gmail.com>
2023-06-12 15:05:32 +0100

    new changes

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,10 @@ import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
-if models.storage_t == "db":
-    Base = declarative_base()
-else:
-    Base = object
+# if models.storage_t == "db":
+#     Base = declarative_base()
+# else:
+#     Base = object
 
 
 class BaseModel:
