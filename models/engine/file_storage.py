cd158c2c8d589df627405e9cbb095d7feaea6b32
Franklin-tech01 <franklinmike2021@gmail.com>
2023-06-12 03:32:27 +0100

    alx 4

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,73 +1,3 @@
-#!/usr/bin/python3
-"""This module defines a class to manage file storage for AirBnB clone"""
-import json
-
-
-# class FileStorage:
-#     """This class manages storage and serializes instances to a
-#     JSON file and deserializes JSON file to instances"""
-#     __file_path = 'file.json'
-#     __objects = {}
-
-#     def all(self, cls=None):
-#         """Returns a dictionary of models currently in storage"""
-#         if cls is not None:
-#             filtered_dict = {}
-#             for key, value in self.__objects.items():
-#                 if isinstance(value, cls):
-#                     filtered_dict[key] = value
-#             return filtered_dict
-#         return FileStorage.
-
-#     def new(self, obj):
-#         """Adds new object to storage dictionary
-#         and set __object to given obj"""
-#         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
-#     def save(self):
-#         """Saves storage dictionary to file"""
-#         with open(FileStorage.__file_path, 'w') as f:
-#             temp = {}
-#             temp.update(FileStorage.__objects)
-#             for key, val in temp.items():
-#                 temp[key] = val.to_dict()
-#             json.dump(temp, f)
-
-#     def reload(self):
-#         """Loads storage dictionary from file"""
-#         from models.base_model import BaseModel
-#         from models.user import User
-#         from models.place import Place
-#         from models.state import State
-#         from models.city import City
-#         from models.amenity import Amenity
-#         from models.review import Review
-
-#         classes = {
-#                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
-#                     'State': State, 'City': City, 'Amenity': Amenity,
-#                     'Review': Review
-#                   }
-#         try:
-#             temp = {}
-#             with open(FileStorage.__file_path, 'r') as f:
-#                 temp = json.load(f)
-#                 for key, val in temp.items():
-#                     self.all()[key] = classes[val['__class__']](**val)
-#         except FileNotFoundError:
-#             pass
-
-#     def delete(self, obj=None):
-#         """Delete obj if it is inside the attribute __objects"""
-#         try:
-#             key = "{}.{}".format(type(obj).__name__, obj.id)
-#             del self.__objects[key]
-#             self.save()
-#         except FileNotFoundError:
-#             pass
-#!/usr/bin/python3
-""" FileStorage module """
-
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -78,25 +8,16 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    # def all(self, cls=None):
-    #     """ Returns the list of objects of one type of class """
-    #     if cls is None:
-    #         return self.__objects
-    #     else:
-    #         objects = {}
-    #         for key, obj in self.__objects.items():
-    #             if type(obj).__name__ == cls.__name__:
-    #                 objects[key] = obj
-    #         return objects
     def all(self, cls=None):
+        """ Returns the list of objects of one type of class """
         if cls is None:
             return self.__objects
         else:
-            filtered_objects = {}
-            for obj_id, obj in self.__objects.items():
-                if type(obj) == cls:
-                    filtered_objects[obj_id] = obj
-            return filtered_objects
+            objects = {}
+            for key, obj in self.__objects.items():
+                if isinstance(obj, cls):
+                    objects[key] = obj
+            return objects
 
     def new(self, obj):
         """ Sets in __objects the obj with key """
@@ -123,77 +44,76 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-    # def delete(self, obj=None):
-    #     """ Deletes obj from __objects if it exists """
-    #     if obj is not None:
-    #         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-    #         if key in self.__objects:
-    #             del self.__objects[key]
-
     def delete(self, obj=None):
+        """ Deletes obj from __objects if it exists """
         if obj is not None:
-            obj_id = obj.__class__.__name__ + "." + obj.id
-            if obj_id in self.__objects:
-                del self.__objects[obj_id]
-
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
 
     def close(self):
-        """Deserializes the JSON file to objects"""
+        """ Deserializes the JSON file to objects """
         self.reload()
 
     def classes(self):
-        """Returns a dictionary of valid classes and their references."""
-        from models.base_model import BaseModel
-        from models.user import User
+        """ Returns a dictionary of valid classes and their references """
         from models.state import State
         from models.city import City
         from models.amenity import Amenity
         from models.place import Place
         from models.review import Review
 
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
+        classes = {
+            'BaseModel': BaseModel,
+            'User': User,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review
+        }
         return classes
 
     def attributes(self):
-        """Returns the valid attributes and their types for classname."""
+        """ Returns the valid attributes and their types for each class """
         attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
+            'BaseModel': {
+                'id': str,
+                'created_at': datetime.datetime,
+                'updated_at': datetime.datetime
+            },
+            'User': {
+                'email': str,
+                'password': str,
+                'first_name': str,
+                'last_name': str
+            },
+            'State': {
+                'name': str
+            },
+            'City': {
+                'state_id': str,
+                'name': str
+            },
+            'Amenity': {
+                'name': str
+            },
+            'Place': {
+                'city_id': str,
+                'user_id': str,
+                'name': str,
+                'description': str,
+                'number_rooms': int,
+                'number_bathrooms': int,
+                'max_guest': int,
+                'price_by_night': int,
+                'latitude': float,
+                'longitude': float,
+                'amenity_ids': list
+            },
+            'Review': {
+                'place_id': str,
+                'user_id': str,
+                'text': str
+            }
         }
-        return attributes
