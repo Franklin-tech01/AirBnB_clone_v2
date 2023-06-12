16c156344ef7b94726e2fc36cec67454a013526f
Franklin-tech01 <franklinmike2021@gmail.com>
2023-06-12 04:07:51 +0100

    updated use

--- models/review.py
+++ models/review.py
@@ -1,22 +1,24 @@
-#!/usr/bin/python3
-"""This is the Review module for the AirBnB project"""
+#!/usr/bin/python
+""" holds class Review"""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review Class to store review information
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
-    __tablename__ = "reviews"
-    text = Column(String(1024),
-                  nullable=False)
-    place_id = Column(String(60),
-                      ForeignKey("places.id", ondelete="CASCADE"),
-                      nullable=False)
-    user_id = Column(String(60),
-                     ForeignKey("users.id", ondelete="CASCADE"),
-                     nullable=False)
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
